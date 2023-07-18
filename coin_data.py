4bdf4afab1de7a75d0336b75a329a6e8f8c1cf55
enhouse <elizabeth.house88@gmail.com>
2023-07-18 10:40:03 -0400

    Initial commit notes only

---
+++ coin_data.py
@@ -0,0 +1,7 @@
+COINS = {
+    "penny": .01,
+    "nickle": .05,
+    "dime": .1,
+    "quarter": .25,
+}
+
---
+++ main.py
@@ -0,0 +1,33 @@
+three coffee flavors needed
+counts how many coffee cups
+espresso =
+generic_coffee = {
+    "name":"Latte",
+   "recipe" = {
+        "milk_in_ml":
+        "coffee_in_grams":
+        "water_in_ml":
+}
+    "price":
+}
+cappacino =
+
+flavors = [
+
+]
+Coffee_machine_resources = {
+    "milk_in_ml":
+    "coffee_in_grams":
+    "water_in_ml":
+    "money":
+}
+
+coin operated
+ takes penny nickle dime quarter
+
+requirements
+    print a report of what it has in stock
+    when user orders need to check it has enough to make drink before taking money
+    if yes ask for payment and then return change at end
+        if not enough money refund
+    make the drink and update the rescourcs
