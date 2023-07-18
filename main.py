b1b9ff8f49802efe38f3edc247f9c38b56b89934
enhouse <elizabeth.house88@gmail.com>
2023-07-18 11:39:55 -0400

    Machine turns off. Machine access report

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
-three coffee flavors needed
-counts how many coffee cups
-espresso =
-generic_coffee = {
-    "name":"Latte",
-   "recipe" = {
-        "milk_in_ml":
-        "coffee_in_grams":
-        "water_in_ml":
-}
-    "price":
-}
-cappacino =
+from machine_data import resources
 
-flavors = [
 
-]
-Coffee_machine_resources = {
-    "milk_in_ml":
-    "coffee_in_grams":
-    "water_in_ml":
-    "money":
-}
+def start_machine():
+    is_on = True
+    while is_on:
+        # Ask the user what they want to do and change it to lowercase
+        user_request = input("“What would you like? (espresso/latte/cappuccino):  ").lower()
+        # TODO 1. print a report of what it has in stock
+        if user_request == "report":
+            # TODO format the resources for printing
+            print(resources)
+            return
+        # TODO Turn off the Coffee Machine by entering “off” to the prompt.
+        elif user_request == "off":
+            is_on = False
+            return
 
-coin operated
- takes penny nickle dime quarter
 
-requirements
-    print a report of what it has in stock
-    when user orders need to check it has enough to make drink before taking money
-    if yes ask for payment and then return change at end
-        if not enough money refund
-    make the drink and update the rescourcs
+start_machine()
+# TODO 2. when user orders need to check it has enough to make drink before taking money
+# TODO 3 takes penny nickle dime quarter
+# TODO 2.1 if yes ask for payment and then return change at end
+# TODO 2.2 if not enough money refund
+# TODO 3. make the drink and update the resources
